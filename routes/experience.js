ce3674ac4f2108396e01cd6db11e821f762a75f1
aude.claquesin <aude.claquesin@gmail.com>
2021-12-09 21:35:30 +0100

    Init New Experience Form

--- routes/experience.js
+++ routes/experience.js
@@ -10,4 +10,9 @@ router.get("/new-experience/create", (req, res, next) => {
     res.render("experience/experience-new");
   });
 
+  router.post("/new-experience/create", (req, res, next) => {
+    console.log(req.body) // ATT on ne récupère pas la catégorie
+    
+  });
+
 module.exports = router;
