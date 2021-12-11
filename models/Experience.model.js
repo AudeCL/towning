1c3dec9a2fcfd1e00416f67f64a6da0c092505e5
aude.claquesin <aude.claquesin@gmail.com>
2021-12-11 15:45:08 +0100

    New Experience form done + Styling

--- models/Experience.model.js
+++ models/Experience.model.js
@@ -2,22 +2,25 @@ const { Schema, model } = require("mongoose");
 
 const experienceSchema = new Schema(
   {
-   user_id: Schema.Types.User,
+   user_id: { type : Schema.Types.ObjectId, ref: 'User' },
    experienceType: {
        type: String, 
        Enum: ['Breakfast', 'Lunch', 'Dinner', 'Happy Hour', 'Running', 'Biking', 'Yoga', 'Extreme Sport', 'Museums', 'Must-see spots', 'Unknown spots', 'Historical spots', 'Other'],
        required: 'Please specifiy the type of experience you want to offer', 
        default: 'Other',
    },
-   experienceDate: Date, 
-   experienceLocation: {
-       experienceCountry: String, 
-       experienceCity: String,
+   experienceDateTime: {
+     type: Date, 
+     required: 'Please specifiy a date for your experience', 
    },
+   experienceLocation: {
+    type: String, 
+    required: 'Please specifiy both country and city for your experience', 
+  },
    experienceDesc: String, 
    experienceImg: {
-       type: String,
-       default: "../public/images/towning-pictures/ExperienceMap.jpg",
+     type: String,
+     default:"https://images.frandroid.com/wp-content/uploads/2016/01/google-maps.png"
    },
    participants: [String],
   },
--- routes/experience.js
+++ routes/experience.js
@@ -1,4 +1,10 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
+const fileUploader = require('../config/cloudinary.config');
+
+
+// Require the Experience model in order to interact with the database
+const Experience = require("../models/Experience.model");
 
 /* GET Search Results (Experiences) page */
 router.get("/search-experience", (req, res, next) => {
@@ -10,9 +16,46 @@ router.get("/new-experience/create", (req, res, next) => {
     res.render("experience/experience-new");
   });
 
-  router.post("/new-experience/create", (req, res, next) => {
-    console.log(req.body) // ATT on ne récupère pas la catégorie
+router.post("/new-experience/create", fileUploader.single('newxpimg'), (req, res, next) => {
+  console.log('CHECK ON OUTPUTS',req.body, req.file)  
+  //ENRICH WITH USER OWNER ID 
+    const experienceType = req.body.newxpcategory;
+    const experienceDateTime = new Date(`${req.body.newxpdate}T${req.body.newxptime}`);
+    const now = new Date(`2022-01-08T15:38:00.000+00:00`);
+    const experienceLocation = req.body.newxpcountrycity;
+    const experienceDesc = req.body.newxpdesc;
+    const experienceImg = req.file.path;
+
+
+    if (!experienceType) {
+      return res
+        .status(400)
+        .render("experience/experience-new", { errorMessage: "Please select an experience type." });
+    }
+
+    /*if (experienceDateTime < now) {
+      return res
+        .status(400)
+        .render("experience/experience-new", { errorMessage: "Please select valid date and time." });
+    }*/
+    //ADD ERROR WHEN NO DATE / TIME SELECTED BY USER
+
+    if (!experienceLocation) {
+      return res
+        .status(400)
+        .render("experience/experience-new", { errorMessage: "Please select a location from the drop down list."});
+    }
     
-  });
+    Experience.create({
+      experienceType, experienceDateTime, experienceLocation, experienceDesc, experienceImg
+    })
+      .then(function (createdExperience) {
+        res.redirect("/"); // REVOIR OU ON FAIT LE REDIRECT A LA SOUMISSION DU FORM
+      })
+      .catch((err) => {
+        console.log(err);
+        res.redirect("/new-experience/create");
+      });
+});
 
 module.exports = router;
