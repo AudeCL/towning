e9c79382263548fc553073efde0f9bae2f4d0d80
Claire Pottier <claire_pottier@hotmail.fr>
2021-12-18 15:39:14 +0100

    delete hote exp implemented

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const User = require("../models/User.model");
 const Experience = require("../models/Experience.model");
+const fileUploader = require('../config/cloudinary.config');
 
 const router = require("express").Router();
 
@@ -14,13 +15,13 @@ router.get("/user-profile/edit", (req, res, next) => {
       });
     })
     .catch(err=> {
-      console.log("Error in updating the user profile");
+      console.log(`Error in updating the user profile: ${err}`);
       next(err)
     })
 });
 
 /* POST EDIT Profile page */
-router.post("/user-profile/edit/:id", (req, res, next) => {
+router.post("/user-profile/edit/:id", fileUploader.single('profileimg'), (req, res, next) => {
   
   User.findByIdAndUpdate(req.params.id, {
     userName: req.body.username,
@@ -29,16 +30,30 @@ router.post("/user-profile/edit/:id", (req, res, next) => {
     userAge: req.body.agegroup,
     userLocation: req.body.location,
     userDesc: req.body.userdescription,
-    userPicture: req.body.userpicture
+    userPicture: req.file
     }, {new: true})
       .then(function(updatedUser) {
         res.redirect(`/user-profile/${updatedUser.id}`)
       })
       .catch(err=> {
-        console.log("Error in updating the user profile");
+        console.log(`Error in updating the user profile: ${err}`);
         next(err)
       })
 });
+
+/*POST Delete Hote Experience dans User profile*/
+router.post('/new-experience/:id/delete', (req, res) => {
+  
+  Experience.findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log(`Hote experience with id: ${req.params.id} deleted`)
+      res.redirect("/user-profile/:id")
+    })
+    .catch(err => {
+      console.log(`Error in deleting the hote experiences in the user profile: ${err}`);
+      next(err)
+    })
+})
 
 /* GET Profile page */
 router.get("/user-profile/:id", (req, res, next) => {
@@ -60,31 +75,19 @@ router.get("/user-profile/:id", (req, res, next) => {
                 })
               })
               .catch(err => {
-                console.log("Error in displaying the hote experiences in the user profile");
+                console.log(`Error in displaying the hote experiences in the user profile: ${err}`);
                 next(err)
               })
           })
           .catch(err=> {
-            console.log("Error in displaying the traveller experiences in the user profile");
+            console.log(`Error in displaying the traveller experiences in the user profile: ${err}`);
             next(err)
           })
     })
     .catch(err=> {
-      console.log("Error in displaying the user profile");
+      console.log(`Error in displaying the user profile: ${err}`);
       next(err)
     })
-
-  /*User.findById(req.session.currentUser._id)
-    .then(function(userFromDB){
-        console.log(userFromDB);
-        res.render("profile/user-profile", {
-            user: userFromDB,
-          })
-        })
-    .catch(err=> {
-      console.log("Error in displaying the user profile");
-      next(err)
-    })*/
 })
 
 module.exports = router;
